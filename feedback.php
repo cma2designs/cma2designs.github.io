2772515e92c87b7b5ecbe1a67b7deaa7bec51689
Caitlin Maloney <caitlin.maloney89@gmail.com>
2024-04-11 14:19:35 -0600

    Add forms table new pages and php

---
+++ feedback.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Thank you <?php echo $_POST["name"]; ?> for your feedback!<br>
+You enjoyed these things the most <?php echo $_POST["most"]; ?><br>
+You thought these things could make things better <?php echo $_POST["improve"]; ?><br>
+You said you were <?php echo $_POST["again"]; ?> out of 5 likely to use the services again.
+
+</body>
+</html> 
